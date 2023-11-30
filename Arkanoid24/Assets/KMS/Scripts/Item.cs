6cd61d9ab0fce27251253cf3571846a633b1b162
alstn1234 <rhalstn8549@gmail.com>
2023-11-30 19:53:27 +0900

    [Feature] Item
    Item 기능 구현

---
+++ Arkanoid24/Assets/KMS/Scripts/Item.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    private float _dropSpeed = 3f;
+    private Rigidbody2D _rb;
+
+    [SerializeField] private Items itemType;
+
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private GameObject ball;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        _rb.velocity = _dropSpeed * Vector3.down;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            ItemSkill(collision.gameObject);
+
+            Destroy(gameObject);
+        }
+    }
+
+    private void ItemSkill(GameObject player)
+    {
+        switch (itemType)
+        {
+            case Items.Player:
+                // 목숨 추가
+                break;
+
+            case Items.Lasers:
+                // 클릭시 2발씩 발사
+                LasersItemUse(player);
+                break;
+
+            case Items.Enlarge:
+                // 패들이 1.5배 커짐(가로)
+                EnalargeItemUse(player);
+                break;
+
+            case Items.Catch:
+                // 공이 튕기지않고 패들에 달라붙음
+                break;
+
+            case Items.Slow:
+                // 공 속도 감소
+                break;
+
+            case Items.Break:
+                // 오른쪽에 탈출구 생성 ->  다음 라운드
+                break;
+
+            case Items.Disruption:
+                // 공 2개 추가
+                break;
+
+        }
+    }
+    private void PlayerItemUse()
+    {
+
+    }
+
+    private void LasersItemUse(GameObject player)
+    {
+        var bullet1 = Instantiate(bullet, player.transform);
+        bullet1.transform.position += new Vector3(-0.5f, 1f, 0f);
+        var bullet2 = Instantiate(bullet, player.transform);
+        bullet2.transform.position += new Vector3(0.5f, 1f, 0f);
+    }
+
+    private void EnalargeItemUse(GameObject player)
+    {
+        var playerScale = player.transform.localScale;
+        player.transform.localScale = new Vector3(playerScale.x * 1.5f, playerScale.y, playerScale.z);
+    }
+
+
+}
