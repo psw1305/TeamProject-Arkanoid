3604d86dee5a67284fce07083562d4f1cc903734
psw1305 <shshck5@gmail.com>
2023-11-30 20:49:34 +0900

    [Add] Arkanoid Ball
    - 공 속도 조절 기능 함수 추가
    - 공 프리팹화

--- Arkanoid24/Assets/2. Script/Game/ArkanoidBall.cs
+++ Arkanoid24/Assets/2. Script/Game/ArkanoidBall.cs
@@ -3,10 +3,19 @@ using UnityEngine;
 public class ArkanoidBall : MonoBehaviour
 {
     [Header("Speed")]
-    [SerializeField] private float ballStartSpeed;
-    [SerializeField] private float ballMaxSpeed;
+    [SerializeField] public float ballStartSpeed;
+    [SerializeField] public float ballMaxSpeed;
 
     private Rigidbody2D ballBody;
+
+    /// <summary>
+    /// Ball 속도 설정 [임시]
+    /// </summary>
+    /// <param name="speed"></param>
+    public void SetMaxSpeed(float speed)
+    {
+        ballMaxSpeed = speed;
+    }
 
     private void Awake()
     {
