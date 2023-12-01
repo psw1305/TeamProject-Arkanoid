244086d17f00dbc28d9df9588ee3dd5818c89c34
psw1305 <shshck5@gmail.com>
2023-12-01 17:19:58 +0900

    [Add] GameManager
    - Current Balls 추가 => 현재 진행되고 있는 공 리스트

--- Arkanoid24/Assets/2. Script/Managers/ArkanoidGame.cs
+++ Arkanoid24/Assets/2. Script/Managers/ArkanoidGame.cs
@@ -1,11 +1,15 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ArkanoidGame
 {
+    public List<GameObject> CurrentBalls = new();
+
     public void InstanceBall()
     {
         var paddle = GameObject.FindWithTag("Player");
         var ballStartPos = new Vector2(paddle.transform.position.x, paddle.transform.position.y + 0.3f);
-        Managers.Resource.Instantiate("BallPrefab", ballStartPos);
+        var ballClone = Managers.Resource.Instantiate("BallPrefab", ballStartPos);
+        CurrentBalls.Add(ballClone);
     }
 }
