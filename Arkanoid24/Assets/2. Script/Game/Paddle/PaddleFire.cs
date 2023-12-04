36312a3bf1bda912a3683774ff249075f1eb6fda
alstn1234 <rhalstn8549@gmail.com>
2023-12-04 12:53:59 +0900

    [Fix] Item
    - Catch 아이템 기능 수정
    - ArkanoidBall.cs-> ballDefaultMaxSpeed 추가

--- Arkanoid24/Assets/2. Script/Game/Paddle/PaddleFire.cs
+++ Arkanoid24/Assets/2. Script/Game/Paddle/PaddleFire.cs
@@ -2,26 +2,23 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class PaddleFire : MonoBehaviour
 {
     private bool _isBallLaunch = false;
-
+    private bool _isBallCatch = true;
     private void Start()
     {
         Managers.Event.OnFireEvent += OnBallFire;
         Managers.Event.OnBallIsLaunch += OnBallIsLaunch;
     }
-
     private void OnBallFire()
     {
-        if (_isBallLaunch) return;
-
+        if (_isBallLaunch && _isBallCatch) return;
         Managers.Event.PublishBallLaunch();
     }
-
     private void OnBallIsLaunch(bool isLaunch)
     {
         _isBallLaunch = isLaunch;
+        _isBallCatch = !isLaunch;
     }
 }
