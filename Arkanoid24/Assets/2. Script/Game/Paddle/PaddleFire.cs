18d331c170b132d8da1801bb5543b02c9ef2fc22
I'm Developer? <59020441+iamdeveloperz@users.noreply.github.com>
2023-12-04 20:27:47 +0900

    [🛠️Fix] - Refactoring
    # Ball.cs
      - 안쓰는 변수 메서드 삭제
      - PaddleController - Start -> Awake로 변경

--- Arkanoid24/Assets/2. Script/Game/Paddle/PaddleFire.cs
+++ Arkanoid24/Assets/2. Script/Game/Paddle/PaddleFire.cs
@@ -1,30 +1,21 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
+
 public class PaddleFire : MonoBehaviour
 {
-    private bool _isBallLaunch = false;
-    private bool _isBallCatch = true;
-
     private void Start()
     {
+        // 좌클릭 이벤트 등록
         Managers.Event.OnFireEvent += OnBallFire;
-        Managers.Event.OnBallIsLaunch += OnBallIsLaunch;
     }
 
     private void OnBallFire()
     {
+        // 남아 있는 볼을 가져온다.
         var remainBall = Managers.Game.CurrentBalls.Find(ball => ball != null).GetComponent<Ball>();
 
         if (remainBall.BallState != BallPreference.BALL_STATE.READY) return;
 
         Managers.Event.PublishBallLaunch();
-    }
-
-    private void OnBallIsLaunch(bool isLaunch)
-    {
-        _isBallLaunch = isLaunch;
-        _isBallCatch = !isLaunch;
     }
 }
