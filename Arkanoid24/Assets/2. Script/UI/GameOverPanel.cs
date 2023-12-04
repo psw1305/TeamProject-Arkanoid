ca610734ba6387745dcfe1821351009e9642c3e4
psw1305 <shshck5@gmail.com>
2023-12-04 19:03:09 +0900

    [Feature] Lobby System
    - 로비 시스템 구현, 메뉴에서 버튼 눌러서 기능 활성화
    - 게임 시작 버튼 누를 시, 스테이지 선택 가능
    - 설정 버튼은 데이터 초기화 가능 => 추후 사운드 조절 기능 추가 예정
    - 크레딧 버튼은 추후 작업 완료시, 명단 작성
    - UI 로비, 메인 프리팹 화
    - 배경 이미지 추가

--- Arkanoid24/Assets/2. Script/UI/GameOverPanel.cs
+++ Arkanoid24/Assets/2. Script/UI/GameOverPanel.cs
@@ -1,18 +1,26 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEditor;
 
 public class GameOverPanel : MonoBehaviour
 {
     [SerializeField] private Button restartButton;
+    [SerializeField] private Button menuButton;
 
     private void Start()
     {
         restartButton.onClick.AddListener(Restart);
+        menuButton.onClick.AddListener(Menu);
     }
 
     public void Restart()
     {
         SceneManager.LoadScene("Main");
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("Lobby");
     }
 }
