09d15497c5b5088712bd7caf044f48a8af28855c
I'm Developer? <59020441+iamdeveloperz@users.noreply.github.com>
2023-12-05 11:50:28 +0900

    [➕Add] - Multi Paddle

--- Arkanoid24/Assets/2. Script/Game/Paddle/Controller/PaddleController.cs
+++ Arkanoid24/Assets/2. Script/Game/Paddle/Controller/PaddleController.cs
@@ -1,5 +1,6 @@
 
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PaddleController : MonoBehaviour
 {
@@ -9,10 +10,59 @@ public class PaddleController : MonoBehaviour
         MOVEMENT
     }
 
-    public PADDLE_STATE PaddleState {  get; set; } = PADDLE_STATE.READY;
+    #region Member Variables
 
+    // Input Field
+    protected PaddleInputHandle _InputHandle;
+    protected PlayerInput _playerInput;
+    protected InputAction _movementAction;
+
+
+    // Physics
+    protected Rigidbody2D _rbody;
+    protected Vector2 _direction;
+    protected float _speed = 10f;
+
+    // Camera
+    [SerializeField] protected Camera _paddleCamera;
+
+    #endregion
+
+
+
+    #region Properties
+    public PADDLE_STATE PaddleState {  get; set; } = PADDLE_STATE.READY;
+    #endregion
+
+
+
+    #region Unity Flow
     private void Awake()
     {
+        _rbody = GetComponent<Rigidbody2D>();
+        _playerInput = GetComponent<PlayerInput>();
+        _InputHandle = new PaddleInputHandle();
+
+        // Service Register
         ServiceLocator.RegisterService(this);
     }
+
+    private void FixedUpdate()
+    {
+        _direction = _movementAction.ReadValue<Vector2>().normalized;
+
+        _rbody.velocity = new Vector2(_direction.x * _speed * Time.fixedDeltaTime, 0);
+    }
+
+    private void OnEnable()
+    {
+        _movementAction = _InputHandle.Paddle.Movement;
+        _InputHandle?.Paddle.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _InputHandle?.Paddle.Disable();
+    }
+    #endregion
 }
