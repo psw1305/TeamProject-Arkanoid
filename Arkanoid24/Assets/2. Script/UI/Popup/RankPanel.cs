17caa591757aa6948e24df2651aa3d9ce5660592
yskybright <rgh99@naver.com>
2023-12-06 12:21:34 +0900

    [Feature] Ranks, NewLevels
    - 랭킹 UI 제작
    - 추가 레벨 10까지 제작

--- Arkanoid24/Assets/2. Script/UI/Popup/RankPanel.cs
+++ Arkanoid24/Assets/2. Script/UI/Popup/RankPanel.cs
@@ -26,7 +26,7 @@ public class RankPanel : MonoBehaviour
     {
         float rankscore = 0;
         rankscore = Managers.Game.Score;
-        PlayerPrefs.SetFloat("Ranks", rankscore) ;
+        PlayerPrefs.SetFloat("Ranks", rankscore);
         float tmpScore = 0f;
 
         for (int i = 0; i < 4; i++)
